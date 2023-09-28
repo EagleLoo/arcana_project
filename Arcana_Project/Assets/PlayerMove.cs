a3d956d056a43a5bd1ebd39939cbe60949cadd56
EagleLoo <l2381924@naver.com>
2023-09-28 21:12:37 +0900

    23.09.28
    플레이어 이동

--- Arcana_Project/Assets/PlayerMove.cs
+++ Arcana_Project/Assets/PlayerMove.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.Examples;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float speed = 0.0001f;
     Vector3 mousePos, transPos, targetPos;
+    SpriteRenderer spriter;
+    Animator anim;
+    private Vector3 m_LastPosition;
 
+    void Start()
+    {
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
@@ -25,5 +34,22 @@ public class PlayerMove : MonoBehaviour
     void MoveToTarget()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime + speed);
+    }
+
+    void FixedUpdate()
+    {
+        anim.SetFloat("Speed", GetSpeed());
+        // 이동하는 방향으로 뒤집기
+        if (transPos.x != 0) {
+            spriter.flipX = transPos.x - transform.position.x < 0;
+        }
+    }
+
+    float GetSpeed()
+    {
+        float speed = (((transform.position - m_LastPosition).magnitude)/Time.deltaTime);
+        m_LastPosition = transform.position;
+
+        return speed;
     }
 }
