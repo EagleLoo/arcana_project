afc520ffe6db6412c45228b7e088c80d242630fa
EagleLoo <l2381924@naver.com>
2023-09-30 01:47:08 +0900

    23.09.30
    캐릭터 연동 실험

--- Arcana_Project/Assets/Scripts/Manager.cs
+++ Arcana_Project/Assets/Scripts/Manager.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviourPunCallbacks
 {
     void Awake()
     {
-        Screen.SetResolution(1080, 1920, false);
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
         PhotonNetwork.ConnectUsingSettings();
+        //SceneManager.LoadScene(0);
     }
 
+    
     // Update is called once per frame
     public override void OnConnectedToMaster() => PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions {MaxPlayers = 3}, null);
 }
